ead4d9cd4e11c208e32fb3d14592a061ec86fc09
srj31 <sourabhrj31@gmail.com>
2020-12-06 18:14:03 +0800

    Name and description edit

--- app/javascript/components/TaskItems/TaskItems.jsx
+++ app/javascript/components/TaskItems/TaskItems.jsx
@@ -1,33 +1,82 @@
 import React, { useState } from 'react';
 import "./TaskItems.css";
+import { Link } from 'react-router-dom';
 
-function TaskItems({task,status,handleSubmit}) {
+function TaskItems({ task, status, handleSubmit }) {
 
-    const [editing,setEditing] = useState(false);
-    const [newTask,setNewTask] = useState(task);
+    const [editing, setEditing] = useState(false);
+    const [newTask, setNewTask] = useState(task);
 
-    const handleChange = (event) => {
+    const handleChangeCompleted = (event) => {
         handleSubmit({
             ...newTask,
             completed: event.target.checked
         })
     }
 
-    return (
-        <div>
-            <div className={`taskItems__section ${status ? "taskItems__completed" : "taskItems__uncompleted"}`}>
-                <div className="taskItems__checkbox">
-                    <input className="form-check-input" type="checkbox" checked={task.completed} value="" id={`checkbox${task.id}`} onChange={handleChange} />
-                </div>
+    const handleEdit = () => {
+        setEditing(true);
+    }
 
-                <div className="taskItems__header">
-                    {task.name}
-                </div>
-                <div className="taskItems__body">
-                    {task.description}
+    const handleChangeName = (event) => {
+        setNewTask({
+          ...newTask,
+          name: event.target.value
+        })
+      }
+
+      const handleChangeDescription = (event) => {
+        setNewTask({
+          ...newTask,
+          description: event.target.value
+        })
+      }
+
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          setEditing(false);
+          handleSubmit(newTask);
+        }
+      }
+
+    return (
+        editing ? (
+            <div className="taskItems__editing">
+                <div className={`taskItems__section ${status ? "taskItems__completed" : "taskItems__uncompleted"}`}>
+                    <div className="taskItems__checkbox">
+                        <input className="form-check-input" type="checkbox" checked={task.completed} value="" id={`checkbox${task.id}`} onChange={handleChangeCompleted} />
+                    </div>
+                    <div className="taskItems__header">
+                        <input type="text" className="form-control-plaintext" id="staticEmail2" value={newTask.name} onChange={handleChangeName} onKeyDown={handleKeyDown} autoFocus/>
+                    </div>
+                    <div className="taskItems__body">
+                        <input type="text" className="form-control-plaintext" id="staticEmail2" value={newTask.description} onChange={handleChangeDescription} onKeyDown={handleKeyDown} autoFocus/>
+                    </div>
+                    <div className="taskItems__links">
+                        <h4> Edit </h4>
+                    </div>
                 </div>
             </div>
-        </div>
+        ) : (
+                <div className="taskItems__normal">
+                    <div className={`taskItems__section ${status ? "taskItems__completed" : "taskItems__uncompleted"}`}>
+                        <div className="taskItems__checkbox">
+                            <input className="form-check-input" type="checkbox" checked={task.completed} value="" id={`checkbox${task.id}`} onChange={handleChangeCompleted} />
+                        </div>
+                        <div className="taskItems__header">
+                            {task.name}
+                        </div>
+                        <div className="taskItems__body">
+                            {task.description}
+                        </div>
+                        <div className="taskItems__links">
+                            <h4 onClick={handleEdit}> Edit </h4>
+                        </div>
+                    </div>
+                </div>
+            )
+
     );
 }
 
