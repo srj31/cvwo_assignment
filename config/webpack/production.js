1eee7d2770724e876a22c7359aed105ea933d2d3
srj31 <sourabhrj31@gmail.com>
2020-12-15 11:53:32 +0800

    update production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const webpack = require("webpack") 
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+
+
 module.exports = environment.toWebpackConfig()
