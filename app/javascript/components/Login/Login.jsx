181ae1961b477c22152c9ecb946d0d744486b3f8
srj31 <sourabhrj31@gmail.com>
2020-12-24 22:08:03 +0800

    Login.css error

--- app/javascript/components/Login/Login.jsx
+++ app/javascript/components/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useHistory, withRouter } from "react-router-dom";
-import "../Login/Login.css";
 import ErrorComp from "../ErrorComp/ErrorComp";
 
 function Login(props) {
