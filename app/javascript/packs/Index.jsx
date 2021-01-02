c54a438e60447843ff0155991b7401a4dc195c11
srj31 <sourabhrj31@gmail.com>
2021-01-02 17:55:26 +0800

    Fixing calendar styling #3.1 /dist/css

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "react-big-calendar/dist/css/react-big-calendar.min.css";
+import "react-big-calendar/dist/react-big-calendar.min.css";
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
