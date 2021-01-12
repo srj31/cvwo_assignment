d144ed5f58ed7ee21aedb6131a6767737541b601
srj31 <sourabhrj31@gmail.com>
2021-01-12 16:46:00 +0800

    add redux store

--- app/javascript/actionTypes.js
+++ app/javascript/actionTypes.js
@@ -1,1 +1,4 @@
-export const FETCH_TASKS = "FETCH_TASKS"
+export const FETCH_TASKS = "FETCH_TASKS";
+export const ADD_TASK = "ADD_TASK";
+export const UPDATE_TASK = "UPDATE_TASK";
+export const DELETE_TASK = "DELETE_TASK";
--- app/javascript/actions/taskActions.js
+++ app/javascript/actions/taskActions.js
@@ -2,10 +2,23 @@ import { FETCH_TASKS } from "../actionTypes";
 
 export const fetchTasks = () => async (dispatch) => {
   const url = "/api/v1/tasks";
-  const res = await fetch(url);
-  const data = res.json();
-  dispatch({
-    type: FETCH_TASKS,
-    payload: data,
-  });
+  // const res = await fetch(url);
+  // const data = res.json();
+  // dispatch({
+  //   type: FETCH_TASKS,
+  //   payload: data,
+  // });
+  await fetch(url)
+  .then(response => {
+    if(response.ok) {
+      dispatch({
+        type: FETCH_TASKS,
+        payload: response.json(),
+      })
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  })
 };
+
