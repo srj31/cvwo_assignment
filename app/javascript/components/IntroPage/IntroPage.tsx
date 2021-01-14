45fd1a95beabc4698c6afda06138ed3bea6a6ec1
srj31 <sourabhrj31@gmail.com>
2021-01-14 13:32:23 +0800

    Animation for Intro page

--- app/javascript/components/IntroPage/IntroPage.tsx
+++ app/javascript/components/IntroPage/IntroPage.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./IntroPage.css";
+import IntroPic from "../../../assets/images/IntroPic.svg";
 
 interface IntroPageProps {}
 
 const IntroPage: React.FC<IntroPageProps> = ({}) => {
   return (
     <div className="introPage container">
-      <h1 style={{ color: "#FFE400" }}> Welcome to DoTO </h1>
       <div className="introPage__body">
-        This is a CRUD application using Rails on the Backend and React as the
-        Frontend Please Signup if you don't have an account or Login to use your
-        own To-Do List
+        <p>
+        <h1 style={{ color: "#FFE400" }}> Welcome to DoTO </h1>
+          This is a CRUD application using Rails on the Backend and React as the
+          Frontend Please Signup if you don't have an account or Login to use
+          your own To-Do List
+        </p>
+        <img
+          src={IntroPic}
+          className="img-fluid move_animation "
+          alt="this me"
+        />
       </div>
     </div>
   );
